8220d8abb8359f65488a12b18b11fd411dcec6bf
James Thang <jamesthang1996@gmail.com>
2020-04-22 23:05:51 +0700

    Initial Commit

---
+++ FInancialCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FInancialCalculator
+//
+//  Created by James Thang on 4/22/20.
+//  Copyright © 2020 James Thang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
